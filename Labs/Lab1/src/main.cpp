b48f7741fff4a60355264ea70ab4e56ed969e049
J-C Borman <borman.johncraig@gmail.com>
2019-02-07 11:38:45 -0500

    Completed Lab1

--- Labs/Lab1/src/main.cpp
+++ Labs/Lab1/src/main.cpp
@@ -1,20 +1,110 @@
 #include "std_lib_facilities.h"
 
-int get_coin_count(const *char coin){
-    int qty = 0;
-    cout << "How many " << coin << " do you have?\n\t:";
-    cin >> qty;
+int get_coin_count(const char* coin){
+    /*
+     * Asks the user for the quantity of coins
+     */
 
-    return qty
+    string qty_str = "";
+    int qty;
+
+    do {
+        cout << "How many " << coin << " do you have?\n\t:";
+        cin >> qty_str;
+
+        try {
+            qty = stoi(qty_str);
+
+            if(qty >= 0){
+                return qty;
+            } else {
+                cout << "The quantity entered: '" << qty << "' is invalid." << endl;
+            }
+
+        } catch (invalid_argument& ia) {
+            cout << "The quantity entered: '" << qty_str << "' is invalid." << endl;
+        }
+
+    } while (true);
 }
 
+bool is_plural(int qty){
+    if(qty == 1){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void print_summary(int p, int n, int d, int q, int hc, int dc){
+    /*
+     * Calculates the total value in cents of all the coins. Prints the summary.
+     */
+    int cents = 0;
+
+    string p_str, n_str, d_str, q_str, hc_str, dc_str;
+
+    // Calculate cents
+    cents = p +
+            (n * 5) +
+            (d * 10) +
+            (q * 25) +
+            (hc * 50) +
+            (dc * 100);
+
+    cout << "You have:" << endl;
+
+    if(is_plural(p)){
+        p_str = "pennies";
+    } else {
+        p_str = "penny";
+    }
+
+    if(is_plural(n)){
+        n_str = "nickels";
+    } else {
+        n_str = "nickel";
+    }
+
+    if(is_plural(d)){
+        d_str = "dimes";
+    } else {
+        d_str = "dime";
+    }
+
+    if(is_plural(q)){
+        q_str = "quarters";
+    } else {
+        q_str = "quarter";
+    }
+
+    if(is_plural(hc)){
+        hc_str = "half-dollars";
+    } else {
+        hc_str = "half-dollar";
+    }
+
+    if(is_plural(dc)){
+        dc_str = "dollars";
+    } else {
+        dc_str = "dollar";
+    }
+
+    cout << "\t" << p << " " << p_str << endl;
+    cout << "\t" << n << " " << n_str << endl;
+    cout << "\t" << d << " " << d_str << endl;
+    cout << "\t" << q << " " << q_str << endl;
+    cout << "\t" << hc << " " << hc_str << endl;
+    cout << "\t" << dc << " " << dc_str << endl;
+    cout << "The value of all your coins is " << cents << endl;
+}
 
 int main()
 {
-    int pennies , nickels, dimes, quarters, hdollars, dollars, cents;
-    pennies = nickels = dimes = quarters = hdollars = dollars = cents = 0;
+    int pennies , nickels, dimes, quarters, hdollars, dollars;
+    pennies = nickels = dimes = quarters = hdollars = dollars = 0;
 
-    // Query for pennies
+    // Query for coins
     pennies = get_coin_count("pennies");
     nickels = get_coin_count("nickels");
     dimes = get_coin_count("dimes");
@@ -22,22 +112,12 @@ int main()
     hdollars = get_coin_count("half-dollars");
     dollars = get_coin_count("dollars");
 
-    // Calculate cents
-    cents = pennies +
-            (nickels * 5) +
-            (dimes * 10) +
-            (quarters * 25) +
-            (hdollars * 50) +
-            (dollars * 100)
-
-    cout << "You have:" << endl;
-    cout << "\t" << pennies << " pennies" << endl;
-    cout << "\t" << nickels << " nickels" << endl;
-    cout << "\t" << dimes << " dimes" << endl;
-    cout << "\t" << quarters << " pennies" << endl;
-    cout << "\t" << hdollars << " quarters" << endl;
-    cout << "\t" << dollars << " dollars" << endl;
-    cout << "The value of all your coins is " << cents << endl
+    print_summary(pennies,
+            nickels,
+            dimes,
+            quarters,
+            hdollars,
+            dollars);
 
     keep_window_open();
     return 0;
